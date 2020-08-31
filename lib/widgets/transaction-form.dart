3f62c2a17d6e70777f3890fcf442f2830fad453b
Uzair Ashraf <uzinatorcl@gmail.com>
2020-08-31 15:20:07 -0700

    transaction form now has some styling, we can move to the chart from here

--- lib/widgets/transaction-form.dart
+++ lib/widgets/transaction-form.dart
@@ -10,21 +10,33 @@ class TransactionForm extends StatelessWidget {
   Widget build(BuildContext ctx) {
     return Card(
       margin: EdgeInsets.all(12),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: titleInput,
-          ),
-          TextField(
-            controller: amountInput,
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-          ),
-          RaisedButton(
-            child: Text('Add Transaction'),
-            onPressed: () => _handleSubmit(title: this.titleInput.text, amount: double.parse(this.amountInput.text)),
-          )
-        ],
+      child: Container(
+        margin: EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: titleInput,
+              decoration: InputDecoration(labelText: 'Expense'),
+            ),
+            TextField(
+              controller: amountInput,
+              decoration: InputDecoration(labelText: 'Cost'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 8, 5, 4),
+              child: RaisedButton(
+                child: Text('Add Transaction', style: TextStyle(color: Colors.white, fontSize: 16),),
+                color: Colors.deepPurple[900],
+                onPressed: () => _handleSubmit(
+                    title: this.titleInput.text,
+                    amount: double.parse(this.amountInput.text)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
