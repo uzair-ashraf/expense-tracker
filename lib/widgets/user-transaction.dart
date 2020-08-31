64c079bb56c87c6f5693e4d944912e06596a3278
Uzair Ashraf <uzinatorcl@gmail.com>
2020-08-31 13:14:28 -0700

    added date time formatter and refinded color scheme

--- lib/widgets/user-transaction.dart
+++ lib/widgets/user-transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
@@ -6,32 +7,51 @@ class UserTransaction extends StatelessWidget {
   final Transaction tx;
   UserTransaction(this.tx);
   Widget build(BuildContext ctx) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(6),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              child: Container(
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(color: Colors.deepPurple[900], fontSize: 20,),
+    return Container(
+      margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.all(6),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                child: Container(
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent[700],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                          color: Colors.deepPurpleAccent[700],
+                          style: BorderStyle.solid,
+                          width: 4.0)),
                 ),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                        color: Colors.purple[400],
-                        style: BorderStyle.solid,
-                        width: 4.0)),
               ),
-            ),
-            Column(
-              children: [Text(tx.title), Text(tx.date.toString())],
-            )
-          ],
+              Column(
+                children: [
+                  Container(
+                    child: Text(
+                      tx.title,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    margin: EdgeInsets.only(bottom: 6),
+                  ),
+                  Text(
+                    DateFormat.yMMMMEEEEd().format(tx.date),
+                    style: TextStyle(color: Colors.blueGrey[400]),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
