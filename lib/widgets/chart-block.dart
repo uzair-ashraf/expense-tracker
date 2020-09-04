9ca8aac8f8cc078af0edfe31ec8597ed03e5a4b4
Uzair Ashraf <uzinatorcl@gmail.com>
2020-09-03 21:36:13 -0700

    moving to laptop

---
+++ lib/widgets/chart-block.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ChartBlock extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return FractionallySizedBox(widthFactor: .14, child: Container(decoration: BoxDecoration(border: Border.all(color: Colors.green)),),)
+  }
+}
