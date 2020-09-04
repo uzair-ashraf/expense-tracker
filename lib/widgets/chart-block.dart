71ef357a3d9f3498a077945281c7bc050b291886
Uzair Ashraf <uzinatorcl@gmail.com>
2020-09-03 21:36:42 -0700

    woops

--- lib/widgets/chart-block.dart
+++ lib/widgets/chart-block.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class ChartBlock extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return FractionallySizedBox(widthFactor: .14, child: Container(decoration: BoxDecoration(border: Border.all(color: Colors.green)),),)
+    return FractionallySizedBox(
+      widthFactor: .14,
+      child: Container(
+        decoration: BoxDecoration(border: Border.all(color: Colors.green)),
+      ),
+    );
   }
 }
